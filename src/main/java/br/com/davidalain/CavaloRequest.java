92f8819ac082b24ed0c7841d286b6488929928a9
matheuscavalcanti97 <matheus_cla@hotmail.com>
2026-07-10 01:03:55 -0300

    feat(security): exigir token de API sem fallback e atualizar pipelines de CI/CD.

---
+++ src/main/java/br/com/davidalain/CavaloRequest.java
@@ -0,0 +1,22 @@
+package br.com.davidalain;
+
+public class CavaloRequest {
+    private int dimensao;
+    private String token; 
+
+    public int getDimensao() {
+        return dimensao;
+    }
+
+    public void setDimensao(int dimensao) {
+        this.dimensao = dimensao;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
